d57df597a1168abe24ac320a437e26e14b1c4f7d
imran <developerhimran@gmail.com>
2022-02-15 00:22:07 +0600

    To Do Funtion Implement Without UI Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-list';
+  title = 'Todo-list'
+  taskValue:any[] = []
+  addTask(val:string){
+    this.taskValue.push({id:this.taskValue.length,name:val});
+    console.warn(this.taskValue);
+  }
+  remoteItem(num:number){
+    this.taskValue.filter(function(item) {
+        return item !== num;
+    })
+    this.taskValue = this.taskValue.filter(val=>val.id !==num)
+  }
+
 }
